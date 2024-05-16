58d1c4ef1c26542a34078c1f80502a5e29daa2f7
Benjent <Benjent@hotmail.fr>
2024-05-16 21:29:59 +0200

    fix(astro.config.mjs): provide site and base properties as repo is not root repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://benjent.github.io/lea-kormas/',
+  base: '/lea-kormas',
   integrations: [tailwind()]
 });
