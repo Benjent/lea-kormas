4ad7641be7c36f2ad4da4a072d8543c9d6605b66
Benjent <Benjent@hotmail.fr>
2024-05-16 21:41:16 +0200

    fix: give up on relative paths and provide repo name as image src prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://benjent.github.io/lea-kormas/',
-  base: './',
+  base: '/lea-kormas',
   integrations: [tailwind()]
 });
